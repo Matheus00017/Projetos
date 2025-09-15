23dec7217b3cab8575150b9f76f1ac89d8455182
Matheus00017 <vidalmatheus321@gmail.com>
2025-09-15 15:55:23 -0300

    incompleto_15-09

--- projéto_numero_1°/javaScript.js
+++ projéto_numero_1°/javaScript.js
@@ -1,27 +1,177 @@
 const receitasLanches = {
     receitasLanches: {
         hamburguer: {
-            nome:"",
-            tempoPreparo:"",
-            porcoes:"",
+            nome: "Hambúrguer Clássico",
+            tempoPreparo:"40 minutos",
+            porcoes:"4 porções",
             ingredientes:[
-
+                "500g de carne moida",
+                "4 pães de hambúrguer",
+                "4 fatias de queijo",
+                "2 tomate fatiado",
+                "8 folhas de alface",
+                "1 cebola roxa fatiada",
+                "Maionese a gosto",
+                "Ketchup a gosto",
+                "Sal e pimenta a gosto",
+                "1 colher de sopa de óleo",
             ],
             modoPreparo:[
-
+                "Tempere a carne moída com sal e pimenta",
+                "Divida a carne em 4 porções e molde os hambúrgueres",
+                "Aqueça uma frigideira com óleo em fogo médio-alto",
+                "Grelhe os hambúrgueres por 4-5 minutos de cada lado",
+                "Adicione o queijo por cima nos últimos minutos",
+                "Corte os pães ao meio e torre levemente",
+                "Monte o lanche: pão, maionese, alface, hambúrguer, tomate, cebola e molhos",
+                "Cubra com a parte superior do pão e sirva"
+            ]
+        },
+        x_salada: {
+            nome: "X-salada",
+            tempoPreparo: "30 minutos",
+            porcoes: "3 porções",
+            ingredientes: [
+                "3 pães de hambúrguer",
+                "300 g de carne moída",
+                "6 fatias de presunto",
+                "2 fatias de queijo",
+                "1 tomate fatiado",
+                "Folhas de alface",
+                "Maionese a gosto",
+                "Sal e pimenta a gosto",
+            ],
+            modoPreparo: [
+                "Tempere carne moída, faça os hambúrgueres",
+                "Grelhe os hambúrgueres até ficarem no ponto desejado",
+                "Torre os pães levemente",
+                "Passe maionese na parte inferior do pão",
+                "Coloque alface, hambúrguer, presunto, queijo e tomate",
+                "Finalize com a parte superior do pão",
+                "Sirva imediatamente" 
             ]
 
         },
-        x_salada: {
+        sanduiche_natural: {
+            nome:"Sanduiche Natural de frango",
+            tempoPreparo: "25 minutoa",
+            porcoes: "3 porções",
+            ingredientes: [
+                "6 fatias de pão integral",
+                "300g de peito de frango desfiado",
+                "2 colheres de sopa de maionese",
+                "1 cenoura ralada",
+                "folhas de alface",
+                "2 tomates fatiados",
+                "Sal e pimenta a gosto",
+            ],modoPreparo: [
+                "Misture o frango desfiado com maionese, sal e pimenta",
+                "Passe manteiga nas fatias de pão (opcional)",
+                "Distribua o frango temperado sobre 2 fatias",
+                "Adicione alface, tomate e cenoura",
+                "Cubra com as outras fatias de pão",
+                "Corte na diagonal e sirva"
+            ]
 
-        },
+        }
 
     }
 }
 const receitasSalgados = {
     receitaSalgados: {
         coxinha: {
+            nome: "Coxinha de frango",
+            tempoPreparo: "1 hora e 30 minutos",
+            porcoes: "20 unidades",
+            ingredientes: [
+                "500g de peito de frango",
+                "2 xícaras de farinha de trigo",
+                "2 xícaras de caldo de frango",
+                "2 colheres de sopa de margarina",
+                "1 cebola picada ",
+                "2 dentes de alho picados",
+                "Sal, pimentas e temperos a gosto",
+                "Farinha de rosca para empanar",
+                "2 ovos batidos",
+                "óleo para fritar",
+            ],
+            modoPreparo: [
+                "Cozinhe o frango e desfie bem miudinho",
+                "Refogue a cebola e alho, adicione o frango e temperos",
 
+                "Para a massa: ferva o caldo com margarina e sal",
+                "Adicione a farinha de uma vez e mexa até desgrudar da panela",
+                "Deixe a massa esfriar até poder manusear",
+
+                "Abra pedaços da massa, recheie com frango",
+                "Modele no formato de coxinha",
+                "Passe no ovo batido e depois na farinha de rosca",
+                "Frite em óleo quente até dourar",
+                "Escorra em papel absorvente e sirva quente"
+            ]
+        },
+        pastel: {
+            nome: "Pastel frito",
+            tempoPreparo: "50 minutos",
+            porcoes: "15 unidades",
+            ingredientes: [
+                "300g de farinha de trigo",
+                "2 ovos",
+                "2 colheres de sopa de óleo",
+                "1 colher de chá de sal",
+                "Água morna (cerca de 150ml)",
+                "300g de queijo muçarela ralado",
+                "Óleo para fritar"
+            ],
+            modoPreparo: [
+                "Misture farinha, sal, ovo e óleo",
+            "Adicione água aos poucos até formar uma massa lisa",
+            "Deixe descansar por 20 minutos",
+            "Abra a massa bem fina com rolo",
+            "Corte retângulos e coloque o queijo no centro",
+            "Feche as bordas pressionando com garfo",
+            "Frite em óleo bem quente até dourar",
+            "Escorra e sirva imediatamente"
+            ]
+        },
+        Empada: {
+            nome: "Empada de Palmito",
+            tempoPreparo: "1 hora",
+            porcoes: "10 unidades",
+            ingredientes: [
+                "2 xícaras de farinha de trigo",
+                "100g de margarina",
+                "1 ovo",
+                "1 colher de chá de sal",
+                "Água gelada",
+                "300g de palmito picado",
+                "1 cebola picada",
+                "2 ovos cozidos picados",
+                "Cheiro-verde a gosto",
+                "Sal e pimenta a gosto",
+            ],
+            modoPreparo: [
+                "Para a massa: misture farinha, sal, margarina e ovo",
+                "Trabalhe até formar uma massa homogênea",
+                "Para o recheio: refogue cebola, palmito e temperos",
+                "Adicione os ovos picados e cheiro-verde",
+                "Abra a massa e forre forminhas de empada",
+                "Adicione o recheio e cubra com mais massa",
+                "Pincele com gema de ovo",
+                "Asse em forno médio (180°C) por 25-30 minutos" 
+            ]
         }
     }
+}
+const receitasBebidas = {
+    bebidasSamAlcool: {
+
+    },
+    bebidasAlcoolicas:{
+        
+    }
+}
+
+const receitasSobremesa = {
+    receitasSobremesa: {}
 }
