5aff8466e0877d4e3d0dee91fb9339ffad14e4c3
Matheus00017 <vidalmatheus321@gmail.com>
2025-10-14 15:43:27 -0300

    inicio_14-10

---
+++ projéto_numero_2º/app.js
@@ -0,0 +1,7 @@
+const btnStart = document.getElementById('btnStart');
+const btnStop =  document.getElementById('btnStop');
+const btnClear = document.getElementById('btnClear');
+const btnSave = document.getElementById('btnSave');
+const statusEl = document.getElementById('status');
+const textoEl = document.getElementById('texto');
+const langSelect = document.getElementById('lang');
